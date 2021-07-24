c2cf75dd6f1596785b60e5e9ee6ac2fbffe42432
teicheld <teicheld@web.de>
2021-07-24 13:32:09 +0200

    add: GHappyTest

--- src/test/java/tudelft/ghappy/GHappyTest.java
+++ src/test/java/tudelft/ghappy/GHappyTest.java
@@ -1,4 +1,21 @@
 package tudelft.ghappy;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class GHappyTest {
+    @ParameterizedTest(name = "str={0}, result={1}")
+    @CsvSource({
+            "'ssgss', false",
+            "'ssggss', true",
+            "'ssggssgss', false",
+            "'ssggssggss', true"
+    })
+    public void isHappyTest(String str, boolean expectedResult)
+    {
+        GHappy happy = new GHappy();
+        boolean result = happy.gHappy(str);
+        Assertions.assertEquals(expectedResult, result);
+    }
 }
